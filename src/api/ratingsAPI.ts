c5ce1625212afc7a74ae1af2950de248d1817563
Michal Sojka <msojka@gohealth.com>
2023-11-02 18:05:53 +0100

    REF-5 Use HTTPS protocol instead of HTTP

--- src/api/ratingsAPI.ts
+++ src/api/ratingsAPI.ts
@@ -34,7 +34,7 @@ export function getEstablishmentRatings(
   pageNum: number
 ): Promise<EstablishmentsType> {
   return fetch(
-    `http://api.ratings.food.gov.uk/Establishments/basic/${pageNum}/10`,
+    `https://api.ratings.food.gov.uk/Establishments/basic/${pageNum}/10`,
     { headers: { "x-api-version": "2" } }
   ).then((res) => res.json());
 }
