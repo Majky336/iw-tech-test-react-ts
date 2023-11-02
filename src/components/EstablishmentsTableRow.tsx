40f8b7fa12fbe12c78570b75496e6267b3d6bc1a
Michal Sojka <msojka@gohealth.com>
2023-11-02 18:10:14 +0100

    USTR-1 Increase table row font size

--- src/components/EstablishmentsTableRow.tsx
+++ src/components/EstablishmentsTableRow.tsx
@@ -1,13 +1,17 @@
 import React from "react";
 import { Establishment } from "../api/ratingsAPI";
 
+const rowStyle: React.CSSProperties = {
+    fontSize: "20px"
+}
+
 export const EstablishmentsTableRow: React.FC<{
   establishment: Establishment;
 }> = ({ establishment }) => {
   return (
     <tr>
-      <td>{establishment?.BusinessName}</td>
-      <td>{establishment?.RatingValue}</td>
+      <td style={rowStyle}>{establishment?.BusinessName}</td>
+      <td style={rowStyle}>{establishment?.RatingValue}</td>
     </tr>
   );
 };
